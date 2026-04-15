6a8e7829d9b72fc1c206544c0ad8ae8b1cd5c561
João Eigenmann <eigenmvnn@outlook.com>
2026-04-15 20:11:10 -0300

    feat: adiciona tela principal, tela pesquisa e reorganiza frontend

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,16 @@
-import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import TelaInicial from './telas/TelaInicial';
-import TelaLogin from './telas/TelaLogin';
 import TelaCadastro from './telas/TelaCadastro';
 import TelaCadastroSucesso from './telas/TelaCadastroSucesso';
+import TelaInicial from './telas/TelaInicial';
+import TelaLogin from './telas/TelaLogin';
 import TelaPerfilAdmin from './telas/TelaPerfilAdmin';
 import TelaPerfilAdminWeb from './telas/TelaPerfilAdminWeb';
+import TelaPesquisa from './telas/TelaPesquisa';
+import TelaPrincipal from './telas/TelaPrincipal';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,9 +22,19 @@ export default function App() {
           <Stack.Screen name="Inicial" component={TelaInicial} />
           <Stack.Screen name="Login" component={TelaLogin} />
           <Stack.Screen name="Cadastro" component={TelaCadastro} />
-          <Stack.Screen name="CadastroSucesso" component={TelaCadastroSucesso} />
+          <Stack.Screen
+            name="CadastroSucesso"
+            component={TelaCadastroSucesso}
+          />
+
+          {/* telas futuras */}
           <Stack.Screen name="PerfilAdmin" component={TelaPerfilAdmin} />
-          <Stack.Screen name="PerfilAdminWeb" component={TelaPerfilAdminWeb} />
+          <Stack.Screen
+            name="PerfilAdminWeb"
+            component={TelaPerfilAdminWeb}
+          />
+          <Stack.Screen name="Principal" component={TelaPrincipal} />
+          <Stack.Screen name="Pesquisa" component={TelaPesquisa} />
         </Stack.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
